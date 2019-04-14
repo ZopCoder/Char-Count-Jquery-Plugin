4e7bdeea70e2cf0442425ab4ddbead9e6dfc9e5f
Zopular Coder <zopluar.coder@gmail.com>
2019-04-14 16:05:37 +0530

    Plugin Body

--- jquery.charCountPlugin.js
+++ jquery.charCountPlugin.js
@@ -1,4 +1,46 @@
-;(function($){
-alert('Hello World');
+//jQuery Plugin to count input characters of a textarea/textbox with remaining characters to show in label.
+;(function ($) {
+    $.fn.charcount = function (options) {
+             var settings = $.extend({}, $.fn.charcount.defaults, options);
+             return this.each(function(){
+                var $this = $(this);
+                var currentValue = $this.text();
+                var ctrl = "<br/><span id='spntxtMult" + $this.attr('id') + "'></span>";
+                $( ctrl ).insertAfter( $this );
+                var limit = $this.attr("data-maxlen");
+                $this.keyup(function () {
+                       var len = $this.val().length;
+                       if (len > limit) {
+						   $this.val($this.val().substring(0, limit));
+                           return;
+                       }
+                       if (len >= (limit - settings.NumOfCharOfAlert)) {
+                           $('#spntxtMult'+ $this.attr('id')).css('color', settings.errFontColor).css('font-weight', settings.FontWeight);
+                           ((limit - len) <= 1) ? $('#spntxtMult'+ $this.attr('id')).text(limit - len + " character left") : $('#spntxtMult'+ $this.attr('id')).text(limit - len + " characters left");
 
-})(JQuery);
+                       }
+                       else {
+                           $('#spntxtMult'+ $this.attr('id')).css('color', settings.okFontColor).css('font-weight', settings.FontWeight);
+                           $('#spntxtMult'+ $this.attr('id')).text(limit - len + " characters left");
+                       }
+
+                   }).focus(function () {
+                          var len = $this.val().length;
+                          if (len >= (limit - settings.NumOfCharOfAlert)) {
+                              $('#spntxtMult'+ $this.attr('id')).css('color', settings.errFontColor).css('font-weight', settings.FontWeight);
+                              ((limit - len) <= 1) ? $('#spntxtMult'+ $this.attr('id')).text(limit - len + " character left") : $('#spntxtMult'+ $this.attr('id')).text(limit - len + " characters left");
+                          }
+                          else {
+                              $('#spntxtMult'+ $this.attr('id')).css('color', settings.okFontColor).css('font-weight', settings.FontWeight);
+                              $('#spntxtMult'+ $this.attr('id')).text(limit - len + " characters left");
+                          }
+                      });
+               });
+        };
+       $.fn.charcount.defaults = {
+           errFontColor: 'red',
+           okFontColor: 'green',
+           FontWeight: 'bold',
+		   NumOfCharOfAlert: 20
+       }
+})(jQuery);
